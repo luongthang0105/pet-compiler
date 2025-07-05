b0fb142f56dee50974f70feb6602a772161aa7ea
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2025-07-05 22:48:10 +1000

    feat: implementing tokenizer fnc and tokens_to_asm fnc

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <sstream>
 #include <optional>
+#include <vector>
 
 enum class TokenType
 {
@@ -9,13 +10,128 @@ enum class TokenType
     int_literal,
     semicolon
 };
-
+std::ostream &operator<<(std::ostream &os, TokenType s)
+{
+    switch (s)
+    {
+    case TokenType::_return:
+        os << "_return";
+        break;
+    case TokenType::int_literal:
+        os << "int_literal";
+        break;
+    case TokenType::semicolon:
+        os << "semicolon";
+        break;
+    default:
+        os << "Unknown Token";
+        break;
+    }
+    return os;
+}
 struct Token
 {
     TokenType type;
     std::optional<std::string> value;
 };
 
+std::ostream &operator<<(std::ostream &output, const Token &token)
+{
+    output << "Token: {type = " << token.type;
+    if (token.value.has_value())
+    {
+        output << ", value = " << token.value.value() << "}";
+    }
+    else
+    {
+        output << "}";
+    }
+    return output;
+}
+auto tokenize(const std::string &str) -> std::vector<Token>
+{
+    auto tokens = std::vector<Token>{};
+    auto buffer = std::string{};
+
+    for (int i = 0; i < str.size(); ++i)
+    {
+        char c = str[i];
+        if (std::isalpha(c))
+        {
+            // got an alpha character, it must be the start of a non-int token
+            // now keep collecting the rest of the token
+            buffer.push_back(c);
+
+            ++i;
+            while (i < str.size() && std::isalnum(str[i]))
+            {
+                buffer.push_back(str[i]);
+                ++i;
+            }
+            --i;
+
+            if (buffer == "return")
+            {
+                tokens.push_back(Token{.type = TokenType::_return, .value = std::nullopt});
+                buffer.clear();
+            }
+        }
+        else if (std::isdigit(c))
+        {
+            buffer.push_back(c);
+
+            ++i;
+            while (i < str.size() && std::isdigit(str[i]))
+            {
+                buffer.push_back(str[i]);
+                ++i;
+            }
+            --i;
+
+            tokens.push_back(Token{.type = TokenType::int_literal, .value = buffer});
+            buffer.clear();
+        }
+        else if (c == ';')
+        {
+            tokens.push_back(Token{.type = TokenType::semicolon, .value = std::nullopt});
+        }
+        else if (std::isspace(c))
+        {
+            continue;
+        }
+        else
+        {
+            std::cerr << "Invalid token: " << c << std::endl;
+            exit(EXIT_FAILURE);
+        }
+    }
+    return tokens;
+}
+
+auto tokens_to_asm(const std::vector<Token> &tokens) -> std::string
+{
+    std::stringstream output;
+    output << "global _start\n_start:\n";
+
+    for (int i = 0; i < tokens.size(); ++i)
+    {
+        if (tokens[i].type == TokenType::_return)
+        {
+            // only proceeds if next 2 tokens are: int_literal then semicolon
+            if (i + 1 >= tokens.size() || tokens[i + 1].type != TokenType::int_literal)
+                continue;
+
+            if (i + 2 >= tokens.size() || tokens[i + 2].type != TokenType::semicolon)
+                continue;
+
+            std::string int_literal = tokens[i + 1].value.value();
+            output << "\tmov rax, 60\n";
+            output << "\tmov rdi, " << int_literal << "\n";
+            i += 2;
+        }
+    }
+    return output.str();
+}
 auto main(int argc, char *argv[]) -> int
 {
     if (argc != 2)
@@ -34,6 +150,10 @@ auto main(int argc, char *argv[]) -> int
         contents = contents_stream.str();
     }
 
-    std::cout << contents << std::endl;
+    auto tokens = tokenize(contents);
+    {
+        auto output = std::ofstream{"hydro.asm", std::ios_base::out};
+        output << tokens_to_asm(tokens);
+    }
     return EXIT_SUCCESS;
 }
